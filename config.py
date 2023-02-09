1dbfc57c24b5fb82b2b3cabdf9ab0fb70c65948e
Haki-Malai <p17mala@ionio.gr>
2023-02-09 15:40:31 +0200

    Add Flask app
    This commit adds a Flask app to the project.

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_TRACK_NOTIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+ 
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.path.join(basedir, "dev-data.sqlite")
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.path.join(basedir, "test-data.sqlite")
+    TESTING = True
+
+    
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.path.join(basedir, "prod-data.sqlite")
+    PRODUCTION = True
+
+
+config = {
+    'default': DevelopmentConfig,
+    'production': ProductionConfig,
+    'development': DevelopmentConfig,
+    'testing': TestingConfig
+}
