aa782071633a563e8f13eac3e5338f452e92b306
Haki-Malai <p17mala@ionio.gr>
2023-02-12 00:50:51 +0200

    Add navigation bar
    - Add navigation bar to base.html
    - Add navigator.html
    - Add nav.html
    - Add navigation to config.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,30 @@
 from flask import current_app, render_template
 from app.main import bp
+from app.models import User, Category, Subcategory, Product
 
 
 @bp.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', config=current_app.config)
+    return render_template('index.html',
+                           config=current_app.config,
+                           Category=Category)
+
+                           
+@bp.route('/products', methods=['GET'])
+def products():
+    return Product.query.first().name
+
+
+@bp.route('/product/<int:product_id>', methods=['GET'])
+def product(product_id):
+    return Product.query.get(product_id).name
+
+
+@bp.route('/category/<int:category_id>', methods=['GET'])
+def category(category_id):
+    return Category.query.get(category_id).name
+
+
+@bp.route('/subcategory/<int:subcategory_id>', methods=['GET'])
+def subcategory(subcategory_id):
+    return Subcategory.query.get(subcategory_id).name
--- config.py
+++ config.py
@@ -5,6 +5,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     # General Configs
     TITLE = "Flask Boilerplate"
+    NAVIGATION = {
+        "Home": "main.index",
+        "Shop": "main.index",
+        "About": "main.index",
+        "Other": {
+            "Subpage": "main.index", 
+            "Subpage2": "main.index",
+            "Subpage3": "main.index",
+            "Subpage4": "main.index",
+            "Subpage5": "main.index",
+        },
+        "Contact": "main.index",
+    }
 
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_TRACK_NOTIFICATIONS = False
