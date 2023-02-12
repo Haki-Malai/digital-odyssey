1b09c016664dcc9821d0661f9cca85702ff1c9ce
Haki-Malai <p17mala@ionio.gr>
2023-02-12 20:37:06 +0200

    Offset cart functionality

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,6 @@
-from flask import current_app, render_template
+from flask import current_app, render_template, jsonify
 from flask_login import current_user, login_required
+from app import db
 from app.main import bp
 from app.models import User, Category, Subcategory, Product
 
@@ -27,6 +28,21 @@ def wishlist():
 @login_required
 def cart():
     return current_user.cart.products
+
+
+@bp.route('/cart/<int:product_id>/<int:quantity>', methods=['GET'])
+@login_required
+def cart_quantity(product_id, quantity):
+    # Update the quantity in the database
+    product = Product.query.get(product_id)
+    current_user.cart.update_quantity(product, quantity)
+    db.session.commit()
+    return jsonify({
+        'product_quantity': quantity,
+        'total_price': current_user.cart.total_price,
+        'product_total_price': current_user.cart.cart_product_total_price(product),
+        'product_total_sale_price': current_user.cart.cart_product_total_sale_price(product),
+    })
 
 
 @bp.route('/checkout', methods=['GET'])
