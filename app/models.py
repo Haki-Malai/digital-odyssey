5c834e7e7eb1d098137df70fd3d0cef0a38a7992
Haki-Malai <p17mala@ionio.gr>
2023-02-12 12:25:31 +0200

    Add cart functionality
    - Add cart model
    - Add cart_products table
    - Add cart methods to User model
    - Add cart methods to Cart model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin, AnonymousUserMixin
+from flask_login import current_user, UserMixin, AnonymousUserMixin
 from app import db, login
 
 
@@ -9,6 +9,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     role = db.Column(db.String(64), default='user')
     password_hash = db.Column(db.String(128))
+
+    cart = db.relationship('Cart', back_populates='user', uselist=False)
 
     def __repr__(self) -> str:
         return '<User %r>' % self.username
@@ -20,7 +22,7 @@ class User(UserMixin, db.Model):
     def password(self):
         raise AttributeError('password is not a readable attribute')
 
-    def set_password(self, password):
+    def password(self, password):
         self.password_hash = generate_password_hash(password)
     
     def verify_password(self, password):
@@ -29,6 +31,20 @@ class User(UserMixin, db.Model):
     # Currently only 2 roles: admin and user so this is fine
     def has_permission(self, permission):
         return self.role == permission
+    
+    # Cart methods
+    def add_to_cart(self, product, quantity):
+        if not self.cart:
+            self.cart = Cart(user=self)
+        self.cart.add_product(product, quantity)
+
+    def remove_from_cart(self, product):
+        if self.cart:
+            self.cart.remove_product(product)
+
+    def empty_cart(self):
+        if self.cart:
+            self.cart.empty_cart()
 
 
 class AnonymousUser(AnonymousUserMixin):
@@ -107,3 +123,45 @@ class Product(db.Model):
 
     def __init__(self, **kwargs):
         super(Product, self).__init__(**kwargs)
+
+
+cart_products = db.Table('cart_products', db.Model.metadata,
+    db.Column('cart_id', db.Integer, db.ForeignKey('cart.id')),
+    db.Column('product_id', db.Integer, db.ForeignKey('product.id'))
+)
+
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    user = db.relationship('User', back_populates='cart')
+    cart_products = db.relationship('CartProduct', back_populates='cart')
+
+    @property
+    def products(self):
+        return [cp.product for cp in self.cart_products]
+
+    @property
+    def total_price(self):
+        return sum(cp.total_price for cp in self.cart_products)
+
+    def add_product(self, product, quantity=1):
+        for cp in self.cart_products:
+            if cp.product == product:
+                cp.quantity += quantity
+                cp.total_price += (quantity * product.price)
+                return
+        cp = CartProduct(cart=self, product=product, quantity=quantity, total_price=(quantity * product.price))
+        db.session.add(cp)
+        
+
+class CartProduct(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cart_id = db.Column(db.Integer, db.ForeignKey('cart.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    quantity = db.Column(db.Integer, nullable=False, default=0)
+    total_price = db.Column(db.Float, nullable=False, default=0.0)
+
+    cart = db.relationship('Cart', back_populates='cart_products')
+    product = db.relationship('Product', lazy='joined')
