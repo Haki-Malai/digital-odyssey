9ba46f4666820e5df1f70c2585ecf1deb6542d1d
Haki-Malai <p17mala@ionio.gr>
2023-03-22 19:55:30 +0200

    Add admin color option
    This commit adds the ability to change the admin color scheme.
    
    The default color scheme is dark.

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,10 +1,15 @@
-from flask import render_template
+from flask import current_app, render_template
 from flask_login import login_required
+
 from app.admin import bp
+from app.models import Category
 from app.decorators import admin_required
+
 
 @bp.route('/admin', methods=['GET'])
 @login_required
 @admin_required
 def index():
-    return render_template('dashboard/index.html')
+    return render_template('admin/index.html',
+                           config=current_app.config,
+                           categories=Category.query.all())
