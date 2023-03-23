971435a8896bcf2834a774053db6015925b7214a
Haki-Malai <p17mala@ionio.gr>
2023-03-23 12:50:56 +0200

    Add admin dashboard
    This commit adds a dashboard to the admin panel.
    
    The dashboard will be the main page of the admin panel.
    
    It will contain a summary of the most important information about the store.
    
    The dashboard will be the first page that the admin will see when he logs in.

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,4 +1,4 @@
-from flask import current_app, render_template
+from flask import current_app, render_template, url_for, redirect
 from flask_login import login_required
 
 from app.admin import bp
@@ -6,10 +6,16 @@ from app.models import Category
 from app.decorators import admin_required
 
 
-@bp.route('/admin', methods=['GET'])
+@bp.route('/admin')
+def index():
+    return redirect(url_for('admin.main', screen='dashboard'))
+
+
+@bp.route('/admin/<screen>')
 @login_required
 @admin_required
-def index():
-    return render_template('admin/index.html',
+def main(screen):
+    return render_template(f'admin/{screen}.html',
+                           screen=screen,
                            config=current_app.config,
                            categories=Category.query.all())
