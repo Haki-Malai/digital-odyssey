8053991c94206372f1e3cc33e111c46749749171
Haki-Malai <p17mala@ionio.gr>
2023-04-17 18:27:21 +0300

    ✨ feat(forms.py): add ColorForm class with string fields for various colors.
    🎨 style(style.css): change variable names to use underscore instead of hyphen
    
    🎨 style(style.css): replace all occurrences of '--border-color2' with '--border-color_2' to follow naming convention.
    
    🎨 style(style.css): change variable names to use underscore instead of hyphen
    🎨 style(style.css): change border color variables to use _ instead of - in name
    🎨 style(style.css): change background color variables to use _ instead of - in name
    
    🎨 style(config.json, colors.html, base.html): change variable names to snake_case convention

---
+++ app/admin/forms.py
@@ -0,0 +1,28 @@
+import re
+from flask import current_app
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import ValidationError
+
+
+class ColorForm(FlaskForm):
+    submit = SubmitField()
+    black_color = StringField()
+    bg_gray_color = StringField()
+    border_color = StringField()
+    border_color_2 = StringField()
+    dark_color = StringField()
+    gray_color = StringField()
+    gray_color_2 = StringField()
+    light_color = StringField()
+    light_color_2 = StringField()
+    secondary_color = StringField()
+    secondary_color_2 = StringField()
+    sky_color = StringField()
+    text_gray_color = StringField()
+    theme_color = StringField()
+    theme_color_2 = StringField()
+    theme_color_3 = StringField()
+    theme_color_4 = StringField()
+    white_color = StringField()
+    yellow_color = StringField()
