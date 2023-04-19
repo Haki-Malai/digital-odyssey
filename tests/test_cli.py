8c6086eb0a7d20b04af2ce39d018f28a2aefb017
Haki-Malai <hakimalai@outlook.com>
2023-04-19 18:11:35 +0300

    🐛 fix(cli.py): remove SQLite database file after running tests 🔥 chore(test_cli.py): remove test_test() function as it is no longer needed

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,20 +1,12 @@
-import click
 from click.testing import CliRunner
 
-from app.cli import fake, test
+from app.cli import fake
 
 
 def test_fake():
     """Test that fake data is generated.
     """
     runner = CliRunner()
+
     result = runner.invoke(fake)
-    assert result.exit_code == 0
-
-
-def test_test():
-    """Test that tests are able to run.
-    """
-    runner = CliRunner()
-    result = runner.invoke(test, ['-c', '-d', 'tests'])
     assert result.exit_code == 0
